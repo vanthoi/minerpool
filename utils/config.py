55ee249bb93a1c2eab0f5488478481e7d0247110
gprethesh <georgeprethesh@georges-MacBook-Air.local>
2024-03-21 14:37:00 +0530

    few changes

--- utils/config.py
+++ utils/config.py
@@ -14,21 +14,21 @@ class Env:
 env = Env()
 
 # Inode Connect Configuration settings
-INODE_IP = "127.0.0.1"
+INODE_IP = "152.53.3.235"
 INODE_PORT = 65432
 BUFFER_SIZE = 1024
 
 
 # MinerPool Configuration settings
-IP = "127.0.0.1"
+IP = "0.0.0.0"
 PORT = 5501
 CHECK_INTERVAL = 600
 MINERPOOL_WALLET_ADDRESS = "DhWyMUj2pna2UYbvrqULyLf6dEo2MNzPHA7Uh4kBrJGFY"
 MINERPOOL_REWARD_WALLET_ADDRESS = "Djxhpx8ogGwpfe1tHxuBLVuxXZEhrS7spstDuXUugJ32i"
-INODE_VALIDATOR_LIST = "http://0.0.0.0:8000/validators"
-CORE_URL = "http://127.0.0.1:3006"
+INODE_VALIDATOR_LIST = "http://152.53.3.235:8000/validators"
+CORE_URL = "https://api.upow.ai"
 PRIVATEKEY = env.PRIVATEKEY
-API_URL = "http://127.0.0.1:3006"
-TRACK = 500
+API_URL = "https://api.upow.ai"
+TRACK = 15616
 FAST_API_URL = "0.0.0.0"
 FAST_API_PORT = 8003
