b55080d36abc71276f42e356ed865ff84eaa4522
gprethesh <georgeprethesh@georges-MacBook-Air.local>
2024-03-27 13:09:16 +0530

    changes

--- utils/config.py
+++ utils/config.py
@@ -13,6 +13,27 @@ class Env:
 
 env = Env()
 
+minerpool_private_key = os.getenv("PRIVATEKEY")
+if minerpool_private_key is None:
+    print(
+        "Minerpool PRIVATEKEY not found. Please check readme.md to set the PRIVATEKEY in the .env variable."
+    )
+    exit(0)
+
+minerpool_wallet_address = os.getenv("MINERPOOLWALLETADDRESS")
+if minerpool_wallet_address is None:
+    print(
+        "Minerpool MINERPOOLWALLETADDRESS not found. Please check readme.md to set the MINERPOOLWALLETADDRESS in the .env variable."
+    )
+    exit(1)
+
+minerpool_reward_address = os.getenv("MINERPOOLREWARDWALLETADDRESS")
+if minerpool_reward_address is None:
+    print(
+        "Minerpool MINERPOOLREWARDWALLETADDRESS not found. Please check readme.md to set the MINERPOOLREWARDWALLETADDRESS in the .env variable."
+    )
+    exit(2)
+
 # Inode Connect Configuration settings
 INODE_IP = "152.53.3.235"
 INODE_PORT = 65432
