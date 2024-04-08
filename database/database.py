a49642710b046c0c7e01d31c39e538496b0f086d
gprethesh <georgeprethesh@georges-MacBook-Air.local>
2024-04-08 20:27:47 +0530

    fix db

--- database/database.py
+++ database/database.py
@@ -1,13 +1,21 @@
 import redis
 import utils.config as config
 
-r = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
+r = redis.Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB,
+    decode_responses=True,
+)
 
 
 def test_redis_connection():
     try:
         r = redis.Redis(
-            host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB
+            host=config.REDIS_HOST,
+            port=config.REDIS_PORT,
+            db=config.REDIS_DB,
+            decode_responses=True,
         )
         # Ping the Redis server
         r.ping()
